e191e6fb182c7d5ad1699f1070af9e4d715038bf
in uk yu <klanoisti@gmai.com>
2020-10-16 12:00:26 +0900

    add Test Weapon0

---
+++ Source/TeamPortfolio/TeamP_Basic/Weapon0.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon0.h"
+
+// Sets default values
+AWeapon0::AWeapon0()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
+	RootComponent = WeaponMesh;
+}
+
+// Called when the game starts or when spawned
+void AWeapon0::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWeapon0::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+
+}
+
---
+++ Source/TeamPortfolio/TeamP_Basic/Weapon0.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon0.generated.h"
+
+UCLASS()
+class TEAMPORTFOLIO_API AWeapon0 : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class USkeletalMeshComponent* WeaponMesh;
+
+public:	
+	// Sets default values for this actor's properties
+	AWeapon0();
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
